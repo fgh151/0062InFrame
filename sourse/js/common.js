e23d9b66df1b06fdf6c5ad2ef7b6ea14b26e993b
fgorsky <fedor@support-pc.org>
2024-11-28 17:10:17 +0300

    tune password field

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -101,14 +101,14 @@ function eventHandler() {
   $(".btn-toggle-type-input").click(function(){
     let icon = $(this).find("svg.icon use")
     let iconId = $(this).find("svg.icon use").attr("xlink:href").split("#")[1];
-
+		let href = $(this).data('href');
 
     const opt = {
       'eye-off':  ['eye','password'],
       'eye':  ['eye-off','text'],
     }
     $(this).parent().find("input").attr("type", opt[iconId][1]);
-    icon.attr("xlink:href",`img/svg/sprite.svg#${opt[iconId][0]}`)
+    icon.attr("xlink:href",`${href}img/svg/sprite.svg#${opt[iconId][0]}`)
   })
 
 
